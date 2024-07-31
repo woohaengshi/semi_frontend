8e5fd1cb461aaa028e4309ef13583d6536d9b46b
Hyunwoo Kang <gusdn741021@gmail.com>
2024-07-31 11:31:48 +0900

    feat: radix세팅

--- app/page.js
+++ app/page.js
@@ -1,8 +1,18 @@
-import Image from "next/image";
+import { Flex, Text, Button } from '@radix-ui/themes';
+
+// function MyApp() {
+//   return (
+//     <Flex direction="column" gap="2">
+//       <Text>Hello from Radix Themes :)</Text>
+//       <Button>Let's go</Button>
+//     </Flex>
+//   );
+// }
 
 export default function Home() {
   return (
     <main >
+      {/* <MyApp /> */}
     </main>
   );
 }
