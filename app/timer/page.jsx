440053ff1389c1a784c0b4d39c0e378e9579ceb0
qbobl5 <ddddd991125@gmail.com>
2024-08-01 11:01:15 +0900

    ss

--- app/timer/page.jsx
+++ app/timer/page.jsx
@@ -1,8 +1,12 @@
+import Subjects from '@/components/subjects'
 import React from 'react'
-
+const subject = ['HTML', 'CSS', 'JS', 'Spring', 'MySQL', 'Oracle', 'React'];
 const Timer = () => {
   return (
-    <div>Timer</div>
+    <div>
+      <div>Timer</div>
+      <Subjects subjects={subject}/>
+    </div>
   )
 }
 
